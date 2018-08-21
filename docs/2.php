8f0fab22103ee76459281bff7b37da85a901c541
vika <vika.elemor@gmail.com>
2018-08-21 16:33:07 +0300

    commit in to new branch

--- docs/2.php
+++ docs/2.php
@@ -1,6 +1,6 @@
 <?
 echo "2.Hello";
-
+echo "2.Hello new branch";
 //hhh
 //hhhh
 //new comment 
--- docs/tt.php
+++ docs/tt.php
@@ -3,8 +3,4 @@ echo "Hello, Git";
 echo "Hello, Git";
 
 echo "Hello, Git";
-echo "Hello, Git";
-echo "Hello, Git";
-echo "Hello, Git";
-echo "Hello, Git";
-echo "Hello, Git";
+echo "Hello, Git. It's me, PHP. Nice to meet you!";
